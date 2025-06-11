4e33c9f9e3397a869967c651ff8a11238f0c26cb
mintoolkit00 <mintoolkit@gmail.com>
2025-06-11 10:12:12 +0200

    email template and function

--- src/email/sendEmail.js
+++ src/email/sendEmail.js
@@ -3,6 +3,8 @@ import { Resend } from 'resend'
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function sendEmail({ to, subject, html }) {
+    console.log('🟢 Starting email function')
+    console.log('Sending to:', to)
     const { error } = await resend.emails.send({
         from: process.env.FROM_EMAIL,
         to,
